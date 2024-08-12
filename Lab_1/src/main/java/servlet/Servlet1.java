486312663d2c1718d6bd2b68eaaaac62505eac39
Ярослав Циба <144675462+YaroslavTsyba@users.noreply.github.com>
2024-08-12 15:26:55 +0300

    Add files via upload

---
+++ Lab_1/src/main/java/servlet/Servlet1.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import entity.Entity;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        Entity entity = new Entity("Entity1", 18, 1.81f);
+        out.println("["+entity+"]");
+    }
+}
